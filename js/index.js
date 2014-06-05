83906b00daea654164db99052b1734cdfb1faee4
Jonas Scheiwiller <jonas.scheiwiller@zhdk.ch>
2014-06-05 02:05:55 +0200

    jquery local / next-tab function / update sidebar
    jquery now also local because of offline internet. next button
    functionality added

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ $( document ).ready(function(){
 
 
 	
-	var active_tab = "klasse"; //active tab as string
+	var active_tab = "vonnach"; //active tab as string
 	
 	
 	function clock() {
@@ -28,25 +28,17 @@ $( document ).ready(function(){
 	
 	/* switch case statement depending on active tab */
 	
-	
-	
-	var click_tab = function ( event ) {
-		var $clicked_elem = $( event.target );
-		var $tab = $clicked_elem.closest( '.tablink' );
-		
-		var $link_name = $tab.attr('name');
-		active_tab = $link_name;
-		
+	var switch_tab = function (){
 		//on every click; remove all active classes and add it to the clicked tab
 		$('.tab.' + active_tab).siblings().removeClass('active');
 	    $('.tab.' + active_tab).addClass('active');
 	    
-	    //make all icons grey
-	    $('.tab img').each(function() {
+	    //make all icons grey except clicked one
+	    $('.tab img').not('.tab.' + active_tab + ' img').each(function() {
 		    var $imgsrc = $( this ).attr('src');
 		    var newsrc = $imgsrc.replace("white", "grey");
 		    $( this ).attr('src', newsrc);
-		    console.log(newsrc);
+/* 		    console.log(newsrc); */
 	    });
 	    
 	    // change icon -> get src as string replace grey > white
@@ -60,33 +52,65 @@ $( document ).ready(function(){
 	    
 	    // show the section belonging to the clicked tab
 	    $('#content > section#' + active_tab ).show();
-	    
-
-		
+	
 		switch (active_tab) {
 		  case "vonnach":
-		    console.log("vonnach");
+		  	$('img.active_triangle').css('top', '65px');
+		    console.log("active: vonnach");
 		    break;
 		  case "via":
-		    console.log("via");
+			$('img.active_triangle').css('top', '183px');
+		    console.log("active: via");
 		    break;
 		  case "datum":
-		    console.log("datum");
+			$('img.active_triangle').css('top', '259px');
+		    console.log("active: datum");
 		    break;
 		  case "einfachretour":
-		    console.log("einfachretour");
+			$('img.active_triangle').css('top', '335px');
+		    console.log("active: einfachretour");
 		    break;
 		  case "klasse":
-		  	console.log("klasse");
+			$('img.active_triangle').css('top', '410px');
+		  	console.log("active: klasse");
 		    break;
-		  case "tickets":
-		    console.log("tickets");
+	      case "tickets":
+			$('img.active_triangle').css('top', '486px');
+		  	console.log("active: tickets");
 		    break;
+		  case "preis":
+			$('img.active_triangle').css('top', '618px');
+		    console.log("active: preis");
+		    break;
+		}
 	}
 	
+	var click_tab = function ( event ) {
+		var $clicked_elem = $( event.target );
+		var $tab = $clicked_elem.closest( '.tablink' );
+		
+		var $link_name = $tab.attr('name');
+		active_tab = $link_name;
+	    
+	    switch_tab();
+		
+			
+	
+	}
+	
+	var next_tab = function ( event ) {
+		var $section_name = $( event.target ).closest('section').attr('id');
+		console.log( $section_name );
+		$('#' + $section_name ).hide();
+		$('#' + $section_name ).next().show();
+		active_tab = $('#' + $section_name ).next().attr('id'); //update active tab
+		switch_tab(); //call switch case function
+		
+		
 	}
 	
 	$( 'sidebar a' ).click( click_tab );
+	$( 'a.next' ).click ( next_tab );
 	
 	
 	
